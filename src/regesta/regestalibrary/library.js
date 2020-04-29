46acf8a7e0a359e426d035d5e88c177bae08760e
Andrea Taroli <andrea.taroli@regestaitalia.it>
2020-04-29 10:22:02 +0000

    1.0.3

--- src/regesta/regestalibrary/library.js
+++ src/regesta/regestalibrary/library.js
@@ -21,7 +21,7 @@ sap.ui.define(["jquery.sap.global",
 		sap.ui.getCore().initLibrary({
 			name: "regesta.regestalibrary",
 			// version: "${version}",
-			version: "1.0.2",
+			version: "1.0.3",
 			dependencies: ["sap.ui.core"],
 			types: [],
 			interfaces: [],
