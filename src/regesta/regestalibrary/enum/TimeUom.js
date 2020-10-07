426a9d1a1382555552e2f6f3400bcef364d33ef3
Andrea Taroli <andrea.taroli@regestaitalia.it>
2020-10-07 12:58:14 +0000

    1.2.1

---
+++ src/regesta/regestalibrary/enum/TimeUom.js
@@ -0,0 +1,23 @@
+/**
+ * Enum for time unit of measure.
+ * @enum {string}
+ */
+sap.ui.define({
+	/** Time uom: seconds. */
+	Second: "Second",
+
+	/** Time uom: minutes. */
+	Minute: "Minute",
+
+	/** Time uom: hours. */
+	Hour: "Hour",
+
+	/** Time uom: days. */
+	Day: "Day",
+
+	/** Time uom: months. */
+	Month: "Month",
+
+	/** Time uom: years. */
+	Year: "Year"
+});
